db7fe664a95d075245bdbb205c4686810c0a26e6
IL <53288945+irlfslab@users.noreply.github.com>
2025-05-05 23:56:13 -0400

    Create app.js

---
+++ server1/app.js
@@ -0,0 +1,15 @@
+const http = require('http');
+const fs = require('fs');
+
+const port = 8080;
+const host = '127.0.0.1';
+
+const server = http.createServer((request, response) => {
+    console.log(request.url);
+    response.write('Test server');
+    response.end();
+})
+
+server.listen(port.host,() => {
+    console.log('ready');
+})
