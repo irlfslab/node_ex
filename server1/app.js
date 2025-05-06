1713fd98647b2fb61d2bc5bbca4e9a03388c2722
IL <53288945+irlfslab@users.noreply.github.com>
2025-05-06 00:51:43 -0400

    Update app.js

--- server1/app.js
+++ server1/app.js
@@ -7,7 +7,7 @@ const host = '127.0.0.1';
 const server = http.createServer((request, response) => {
     console.log(request.url);
     response.writeHead(200,{'Content-Type':'text/html'});
-    response.write('<h1>Test Server 1</h1>');
+    response.write('<h1>Test Server</h1>');
     response.write(`Request URL : ${request.url}`);
     response.end();
 })
