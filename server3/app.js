dc2fbc12b4154decc4bd50c3ab1e1fe879c2519b
IL <53288945+irlfslab@users.noreply.github.com>
2025-05-13 01:34:10 -0400

    Create app.js

---
+++ server3/app.js
@@ -0,0 +1,30 @@
+const http = require('http');
+const fs = require ('fs');
+const url = require ('url');
+
+const port = 8080;
+const host = '127.0.0.1';
+
+const server = http.createServer((request, response) => {
+    const base = url.parse(request.url);
+    //console.log(base);
+    const fileName = './mod' + base.pathname;
+    fs.readFile(fileName, (error, html) => {
+        if (error) {
+            response.writeHead(404, {
+                'Content-Type': 'text/html'
+        });
+        response.end(`<h1>Not found </h1>`);
+    }
+     else {response.writeHead(200, {
+                'Content-Type': 'text/html'
+            });
+            response.write(html);
+            response.end();
+        }
+    })
+})
+
+server.listen(port,host,() => {
+    console.log('ready');
+})
